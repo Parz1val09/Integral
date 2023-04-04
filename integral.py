db27993a62cfa529ea1b0f15234d9a052758da51
Danilo Dias <75582033+Parz1val09@users.noreply.github.com>
2023-04-04 15:51:06 -0400

    Update integral.py

--- integral.py
+++ integral.py
@@ -1,1 +1,13 @@
-print('Hello World!')
+def f(x):
+  return x**4 + 2*x**3 - x**2 + 9*x
+
+def integral_definida(a, b, n):
+  h = (b-a)/n
+  sum = 0
+  for i in range(1, n):
+    sum += f(a + i*h)
+  return h/2 * (f(a) + f(b) + 2*sum)
+
+result = integral_definida(0, 1, 1000)
+print(round(result, 5))
+  
